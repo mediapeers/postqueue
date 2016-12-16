bce0e7ce3bb0ceed95d68b674f22b2ea58b3eebd
eno <eno@open-lab.org>
2016-12-16 11:58:36 +0100

    Adds specs for wildcard processing

---
+++ spec/postqueue/wildcard_spec.rb
@@ -0,0 +1,31 @@
+require "spec_helper"
+
+describe "enqueuing" do
+  let(:callback_invocations) { @callback_invocations ||= [] }
+
+  let(:queue) do
+    Postqueue.new do |queue|
+      queue.on "*" do |op, entity_ids|
+        callback_invocations << [ op, entity_ids ]
+      end
+    end
+  end
+
+  let(:items) { queue.item_class.all }
+  let(:item)  { queue.item_class.first }
+
+  context "when enqueuing in sync mode" do
+    before do
+      queue.enqueue op: "op", entity_id: 12
+      queue.process
+    end
+
+    it "processed the items" do
+      expect(callback_invocations.length).to eq(1)
+    end
+
+    it "removed all items" do
+      expect(items.count).to eq(0)
+    end
+  end
+end
