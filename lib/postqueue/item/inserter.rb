afc75ddfafefa0c14d5634f893b62697b3a719d6
eno <eno@open-lab.org>
2017-01-18 12:55:52 +0100

    Minor performance improvement

--- lib/postqueue/item/inserter.rb
+++ lib/postqueue/item/inserter.rb
@@ -28,13 +28,12 @@ module Postqueue
         "INSERT INTO #{table_name}(op, entity_id) VALUES($1, $2)"
       end
 
-      def prepared_inserter_statement
+      def prepared_inserter_statement(raw_connection)
         @prepared_inserter_statements ||= {}
 
         # a prepared connection is PER DATABASE CONNECTION. It is not shared across
         # connections, and it is not per thread, since a Thread might use different
         # connections during its lifetime.
-        raw_connection = connection.raw_connection
         @prepared_inserter_statements[raw_connection.object_id] ||= create_prepared_inserter_statement(raw_connection)
       end
 
@@ -46,12 +45,14 @@ module Postqueue
       end
 
       def insert_item(op:, entity_id:)
-        connection.raw_connection.exec_prepared(prepared_inserter_statement, [op, entity_id])
+        raw_connection = connection.raw_connection
+        statement_name = prepared_inserter_statement(raw_connection)
+        raw_connection.exec_prepared(statement_name, [op, entity_id])
       end
     end
 
     # extend ActiveRecordInserter   # 600µs per item
     extend RawInserter              # 100µs per item
-    #extend PreparedRawInserter     # 50µs per item
+    # extend PreparedRawInserter    # 50µs per item
   end
 end
