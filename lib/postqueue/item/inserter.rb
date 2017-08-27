d32acb4c9a8b4dc09f7a9dba6d6a43b602521fd6
eno <eno@open-lab.org>
2017-08-27 23:17:28 +0200

    Refactor inserters to support variable attributes

--- lib/postqueue/item/inserter.rb
+++ lib/postqueue/item/inserter.rb
@@ -8,51 +8,33 @@ module Postqueue
   # Which one will be used depends on the "extend XXXInserter" line below.
   class Item < ActiveRecord::Base
     module ActiveRecordInserter
-      def insert_item(op:, entity_id:)
-        create!(op: op, entity_id: entity_id)
+      def insert_item(attrs)
+        create!(attrs)
       end
     end
 
     module RawInserter
+      def insert_item(attrs)
+        values = attrs.values_at *queue_attribute_names
+        connection.raw_connection.exec_params(insert_sql, values)
+      end
+
+      private
+
+      def queue_attribute_names
+        [ :op, :entity_id ]
+      end
+
       def insert_sql
-        "INSERT INTO #{table_name}(op, entity_id) VALUES($1, $2)"
-      end
-
-      def insert_item(op:, entity_id:)
-        connection.raw_connection.exec_params(insert_sql, [op, entity_id])
-      end
-    end
-
-    module PreparedRawInserter
-      def insert_sql
-        "INSERT INTO #{table_name}(op, entity_id) VALUES($1, $2)"
-      end
-
-      def prepared_inserter_statement(raw_connection)
-        @prepared_inserter_statements ||= {}
-
-        # a prepared connection is PER DATABASE CONNECTION. It is not shared across
-        # connections, and it is not per thread, since a Thread might use different
-        # connections during its lifetime.
-        @prepared_inserter_statements[raw_connection.object_id] ||= create_prepared_inserter_statement(raw_connection)
-      end
-
-      # prepares the INSERT statement, and returns its name
-      def create_prepared_inserter_statement(raw_connection)
-        name = "postqueue-insert-#{table_name}-#{raw_connection.object_id}"
-        raw_connection.prepare(name, insert_sql)
-        name
-      end
-
-      def insert_item(op:, entity_id:)
-        raw_connection = connection.raw_connection
-        statement_name = prepared_inserter_statement(raw_connection)
-        raw_connection.exec_prepared(statement_name, [op, entity_id])
+        @insert_sql ||= begin
+          columns = queue_attribute_names.map(&:to_s)
+          placeholders = 1.upto(columns.count).map { |i| "$#{i}" } 
+          "INSERT INTO #{table_name}(#{columns.join(", ")}) VALUES(#{placeholders.join(", ")})"
+        end
       end
     end
 
     # extend ActiveRecordInserter   # 600µs per item
-    extend RawInserter              # 100µs per item
-    # extend PreparedRawInserter    # 50µs per item
+    extend RawInserter              # 200µs per item
   end
 end
