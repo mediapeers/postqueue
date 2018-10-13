e833e9c26447f91a640e01bec67cf56bc7d56763
eno <eno@open-lab.org>
2018-10-13 10:15:00 +0100

    Updates VERSION and release management scripts

--- lib/postqueue/version.rb
+++ lib/postqueue/version.rb
@@ -1,3 +1,24 @@
 module Postqueue
-  VERSION = "0.7.2"
+  module GemHelper
+    extend self
+
+    def version(name)
+      spec = Gem.loaded_specs[name]
+      version = spec ? spec.version.to_s : "0.0.0"
+      version += "+unreleased" if unreleased?(spec)
+      version
+    end
+
+    private
+
+    def unreleased?(spec)
+      return false unless defined?(Bundler::Source::Gemspec)
+      return true if spec.source.is_a?(::Bundler::Source::Gemspec)
+      return true if spec.source.is_a?(::Bundler::Source::Path)
+
+      false
+    end
+  end
+
+  VERSION = GemHelper.version "postqueue"
 end
